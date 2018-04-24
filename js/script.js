5fbc8b188fdd5400b082a610690f79ee31cdd918
xc233 <quantsleeper@gmail.com>
2018-04-24 10:29:51 +0800

    Site updated: 2018-04-24 10:29:50

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@
         var bannerNode = $('.top-image');
         if(bannerNode.data('enable')){
             // bannerNode.attr('style','background-image:url(https://ws1.sinaimg.cn/large/dd0c8f65ly1fplk0jszl7j21hc0u0wik.jpg)');
-            bannerNode.attr('style','background-image:url(https://ws1.sinaimg.cn/large/dd0c8f65ly1fp5gs10x87j21hc0u07fk.jpg)');
+            bannerNode.attr('style','background-image:url(https://ws1.sinaimg.cn/large/0072A1Xdly1fqnjcq2lrnj31221jkapq.jpg)');
         }
     })
 
